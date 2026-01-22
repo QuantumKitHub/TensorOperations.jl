b3947d2b88c3a723384c65e1070416134d84bcb1
Ebel Nikolay <ebelnikola@gmail.com>
2026-01-22 12:44:47 +0100

    _kron bug fixed

--- src/utils.jl
+++ src/utils.jl
@@ -1,8 +1,8 @@
 _kron(Es::NTuple{1}, ba) = Es[1]
-function _kron(Es::NTuple{N, Any}, ba) where {N}
+function _kron(Es::NTuple{N,Any}, ba) where {N}
     E1 = Es[1]
     E2 = _kron(Base.tail(Es), ba)
     p2 = trivialpermutation(0, 2N - 2)
     p = ((1, (2 .+ trivialpermutation(N - 1))...), (2, ((N + 1) .+ trivialpermutation(N - 1))...))
-    return tensorproduct(p, E1, ((1, 2), ()), false, E2, p2, false, One(), ba...)
+    return tensorproduct(E1, ((1, 2), ()), false, E2, p2, false, p, One(), ba...)
 end
