880d046330a665488f6592a6e6979fdd8af665ec
Katharine Hyatt <kshyatt@users.noreply.github.com>
2026-05-12 03:39:18 +0200

    Try to test Enzyme on 1.10 (#270)
    * Try to test Enzyme on 1.10
    
    * Remove nonexistent test
    
    * Fix forward mode returns

--- test/runtests.jl
+++ test/runtests.jl
@@ -39,13 +39,10 @@ if !is_buildkite
     @testset "mooncake" verbose = false begin
         include("mooncake.jl")
     end
-    # mystery segfault on 1.10 for now
-    @static if VERSION >= v"1.11.0"
-        is_apple_ci = Sys.isapple() && get(ENV, "CI", "false") == "true"
-        if !is_apple_ci
-            @testset "enzyme" verbose = false begin
-                include("enzyme.jl")
-            end
+    is_apple_ci = Sys.isapple() && get(ENV, "CI", "false") == "true"
+    if !is_apple_ci
+        @testset "enzyme" verbose = false begin
+            include("enzyme.jl")
         end
     end
 end
